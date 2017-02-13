d87d9d7ebbb133941a47f1f16d171def0201762d
Brian Kaiser <brian.kaiser@hudl.com>
2017-02-13 16:34:02 -0500

    Get offline IoT caching working

--- src/iot-shadow/index.js
+++ src/iot-shadow/index.js
@@ -1,8 +1,15 @@
 const bunyan = require('bunyan');
+const fs = require('fs');
 const path = require('path');
 const os = require('os');
 const thingShadow = require('aws-iot-device-sdk').thingShadow;
 const _ = require('lodash');
+const deferred = require('deferred');
+const yaml = require('js-yaml');
+
+const promisify = deferred.promisify;
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
 
 (function() {
@@ -10,12 +17,12 @@ const _ = require('lodash');
   let deviceState = {};
   let logger;
   let connected = false;
+  let thingShadows;
 
   function initIotShadowConnection() {
     //TODO: verify host, keys exist, deviceId exists
 
-    console.log(path.resolve('../certs', 'thing.private.key'));
-    const thingShadows = thingShadow({
+    thingShadows = thingShadow({
       keyPath: path.resolve('../certs', 'thing.private.key'),
       certPath: path.resolve('../certs',  'thing.cert.pem'),
       caPath: path.resolve('../certs',  'root-ca.crt'),
@@ -26,37 +33,32 @@ const _ = require('lodash');
       delay: 4000,
       protocol: 'mqtts',
       host: localConfig.iotHostname,
-      debug: true
+      debug: false,
     });
     thingShadows.register(localConfig.deviceId, {
         persistentSubscribe: true
     });
+
     thingShadows.on('connect', function() {
       logger.info('aws iot connected');
-      connected = true;
+
       setTimeout(function(){
         thingShadows.get(localConfig.deviceId); //trigger an initial get
-      }, 2000);
+      }, 3000);
     });
     thingShadows.on('close', function() {
       logger.info('aws iot closed');
       connected = false;
-      thingShadows.unregister(localConfig.deviceId);
     });
-    thingShadows.on('reconnect', function() {
-      thingShadows.register(localConfig.deviceId, {
-        persistentSubscribe: true
-      });
-      connected = true;
-      setTimeout(function(){
-        thingShadows.get(localConfig.deviceId); //trigger a refresh get
-      }, 2000);
+    thingShadows.on('reconnect', function(a,b) {
+      logger.debug('aws iot reconnect attempt');
     });
     thingShadows.on('message', function(topic, payload) {
       logger.info('aws iot message', topic, payload);
     });
     thingShadows.on('delta', function(thingName, stateObject) {
       logger.debug('aws iot delta', thingName, stateObject);
+      connected = true;
       process.send({
           type: 'DeviceStateChanged',
           payload: _.merge({}, deviceState, stateObject.state),
@@ -64,6 +66,7 @@ const _ = require('lodash');
     });
     thingShadows.on('status', function(thingName, statusType, clientToken, stateObject) {
       logger.debug('awsstatus', thingName, statusType, clientToken, stateObject);
+      connected = true;
       if(statusType === 'accepted' && thingName === localConfig.deviceId) {
         //set device state
         process.send({
@@ -72,6 +75,41 @@ const _ = require('lodash');
         });
       }
     });
+    thingShadows.on('error', function(err) {
+      if(err.code === 'ENOTFOUND') {
+        logger.debug({err:err}, 'Remote host not found. Probably no internet connection');
+        return;
+      }
+
+      logger.error({err:err});
+    })
+  }
+  function startOfflineStateRetrievalTimer() {
+
+    //if we don't get the state in 30s, then assume we can't connect
+    //and attempt to load the offline cached state
+    setTimeout(function() { 
+      if(!_.isEmpty(deviceState)) {
+        return;
+      }
+
+      const cachedConfigPath = path.join(localConfig.tmpDirectory, 'cached-device-state.yaml');
+      logger.info({configPath:cachedConfigPath}, 'Loading cached device state.')
+      readFileAsync(cachedConfigPath, 'utf8')
+        .then(function(result){
+          return yaml.safeLoad(result);
+      }).done(function(config) {
+            //set device state
+            process.send({
+              type: 'DeviceStateChanged',
+              payload: _.merge({}, config),
+            });
+        }, function(err){
+          logger.warn({configPath:cachedConfigPath, err:err}, 'Attempted to load cached state and it failed');
+        });
+      
+
+    }, 30000);
   }
 
   function init(config) {
@@ -93,10 +131,16 @@ const _ = require('lodash');
     });
 
     initIotShadowConnection();
+
+    startOfflineStateRetrievalTimer();
   }
 
   function ProcessUpdatedDeviceState(state) {
     deviceState = state;
+    //save cached version to disk
+    const cachedConfigPath = path.join(localConfig.tmpDirectory, 'cached-device-state.yaml');
+    const yamlState = yaml.safeDump(deviceState);
+    writeFileAsync(cachedConfigPath, yamlState).done();
   }
   function buildIotStatusMessage() {
     return {
