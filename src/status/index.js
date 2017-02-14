dc459a9c808a871d30d184ff9715d9fdae22b370
Brian Kaiser <brian.kaiser@hudl.com>
2017-02-14 16:12:31 -0500

    Added camera ping to status

--- src/status/index.js
+++ src/status/index.js
@@ -1,110 +1,116 @@
-const bunyan = require('bunyan');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const dns = require('dns');
-const deferred = require('deferred');
-const _ = require('lodash');
+const bunyan = require('bunyan')
+const path = require('path')
+const os = require('os')
+const dns = require('dns')
+const deferred = require('deferred')
+const ping = require('ping')
+const _ = require('lodash')
 
-const promisify = deferred.promisify;
-const readDirAsync = promisify(fs.readdir);
-const unlinkAsync = promisify(fs.unlink);
-const lookupAsync = promisify(dns.lookup);
+const promisify = deferred.promisify
+const lookupAsync = promisify(dns.lookup)
 
 
-(function() {
-  let localConfig = {};
-  let deviceState = {};
-  let currentStatus = {};
-  let logger;
-  let currentExternalIp = '';
+;(function () {
+  let localConfig = {}
+  let deviceState = {}
+  let currentStatus = {}
+  let logger
+  let currentExternalIp = ''
+  let currentCameraPing = false
+  let currentInternalIps = ''
 
 
-  function init(config) {
-    localConfig = config;
+
+  function init (config) {
+    localConfig = config
     logger = bunyan.createLogger({
       name: 'status-log',
       deviceId: localConfig.deviceId,
       streams: [{
-          type: 'rotating-file',
-          level: 'info',
-          path: path.join(config.loggingPath, 'status-log.log'),
-          period: '1d',   // daily rotation
-          count: 3        // keep 3 back copies
+        type: 'rotating-file',
+        level: 'info',
+        path: path.join(config.loggingPath, 'status-log.log'),
+        period: '1d',   // daily rotation
+        count: 3        // keep 3 back copies
       },
       {
         stream: process.stdout,
         level: 'debug'
       }]
-    });
-
+    })
   }
 
-  function UpdateStatus(payload) {
-    currentStatus = _.merge(currentStatus, payload);
+  function UpdateStatus (payload) {
+    currentStatus = _.merge(currentStatus, payload)
   }
 
-  function ProcessUpdatedDeviceState(newState) {
-    let prevState = deviceState;
-    deviceState = newState;
-
+  function ProcessUpdatedDeviceState (newState) {
+    deviceState = newState
   }
 
-  process.on('message', function(msg) {
-    if (!msg) return;
+  process.on('message', function (msg) {
+    if (!msg) return
 
     if (msg.type === 'Init') {
-      init(msg.payload);
+      init(msg.payload)
     } else if (msg.type === 'DeviceStateChanged') {
-      ProcessUpdatedDeviceState(msg.payload);
-    } else if(msg.type === 'StatusUpdate') {
-      UpdateStatus(msg.payload);
+      ProcessUpdatedDeviceState(msg.payload)
+    } else if (msg.type === 'StatusUpdate') {
+      UpdateStatus(msg.payload)
     }
   })
 
-  function GatherInternalStatuses() {
-    // get local IPs
-    const internalIps = 
-      _.chain(os.networkInterfaces())
-      .flatMap()
-      .filter(function(ni) {
-        return !ni.internal && ni.family === 'IPv4';
-      }).map(function(ni) {
-        return ni.address
-      }).join(', ')
-      .value();
-
-      lookupAsync('myip.opendns.com',{family: 4})
-      .then(function(addresses) {
-        currentExternalIp = addresses[0];
+  function GatherInternalStatuses () {
+      // look up external IP
+    return lookupAsync('myip.opendns.com', {family: 4})
+      .then(function (addresses) {
+        currentExternalIp = addresses[0]
       })
-      .done(function() {
-
-      }, function(err) {
-
-      });
-
-    // get external IP
-    // get CPU
-    // get disk percentage on tmp drive
-    // get camera info? connection status? stream status?
-    const statusObj = {
-      deviceId: localConfig.deviceId,
-      internalIps: internalIps,
-      externalIp: currentExternalIp,
-      freeMemory: os.freemem() / (1024*1024),
-      loadAverage: _.join(os.loadavg(), ', '),
-    };
-    currentStatus = _.merge(currentStatus, statusObj);
+      .then(function () {
+        // camera ping
+        if (deviceState && !!deviceState.cameraIp) {
+          return ping.promise.probe(deviceState.cameraIp)
+        }
+      })
+      .then(function (cameraPingResult) {
+        if (!cameraPingResult) return
+        // record camera ping
+        currentCameraPing = cameraPingResult.alive
+      })
+      .then(function () {
+        // get local IPs
+        currentInternalIps =
+          _.chain(os.networkInterfaces())
+          .flatMap()
+          .filter(function (ni) {
+            return !ni.internal && ni.family === 'IPv4'
+          }).map(function (ni) {
+            return ni.address
+          }).join(', ')
+          .value()
+      })
+      .then(function () {
+        const statusObj = {
+          deviceId: localConfig.deviceId,
+          internalIps: currentInternalIps,
+          externalIp: currentExternalIp,
+          freeMemory: os.freemem() / (1024 * 1024),
+          loadAverage: _.join(os.loadavg(), ', '),
+          cameraPing: currentCameraPing
+        }
+        return _.merge(currentStatus, statusObj)
+      })
   }
 
-  let statusInterval = setInterval(function() {
-    //update internal status
-    GatherInternalStatuses();
-    process.send({
-      type: 'CompiledStatus',
-      payload: currentStatus
-    });
-  }, 10000);
-
-})();
+  setInterval(function () {
+    // update internal status
+    GatherInternalStatuses().done(function (fullStatus) {
+      process.send({
+        type: 'CompiledStatus',
+        payload: currentStatus
+      })
+    }, function (err) {
+      logger.warn(err, 'error compiling status')
+    })
+  }, 10000)
+})()
