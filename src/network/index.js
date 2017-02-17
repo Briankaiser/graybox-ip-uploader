65f3e81fe67069d66bc2d6796591308ce136a2c0
Brian Kaiser <brian.kaiser@hudl.com>
2017-02-17 16:01:27 -0500

    First attempt on actually creating iptables

--- src/network/index.js
+++ src/network/index.js
@@ -1,11 +1,10 @@
 const bunyan = require('bunyan')
 const path = require('path')
 const deferred = require('deferred')
-// const async = require('async')
-// const _ = require('lodash')
+const childProcess = require('child_process')
 
-// const promisify = deferred.promisify
-// const lookupAsync = promisify(dns.lookup)
+const IPTABLES_TABLE = 'nat'
+const IPTABLES_CHAIN = 'PREROUTING'
 
 ;(function () {
   let localConfig = {}
@@ -22,6 +21,72 @@ const deferred = require('deferred')
       clearInterval(statusInterval)
     }
   })
+
+  function executeIpTablesCommand (args) {
+    let d = deferred()
+    const proc = childProcess.spawn('iptables', args)
+    proc.on('close', (code) => {
+      if (code !== 0) {
+        d.reject(new Error(`Process returned exit code: ${code}`))
+      } else {
+        d.resolve()
+      }
+    })
+    return d.promise
+  }
+  function executeFlushChain () {
+    const args = [
+      '-t',
+      IPTABLES_TABLE,
+      '-F',
+      IPTABLES_CHAIN
+    ]
+    return executeIpTablesCommand(args)
+  }
+  function executeAddCameraForwardPort (port) {
+    const cameraIpAndPort = deviceState.cameraIp + ':' + port
+    const args = [
+      '-t',
+      IPTABLES_TABLE,
+      '-A',
+      IPTABLES_CHAIN,
+      '-j', // interface to forward from
+      'eth0',
+      '-p', // protocol
+      'tcp',
+      '--dport', // port to route from (destination of original request)
+      port,
+      '-j', // jump. destination routing area?
+      'DNAT',
+      '--to-destination',
+      cameraIpAndPort
+    ]
+    return executeIpTablesCommand(args)
+  }
+
+  function turnProxyOff () {
+    logger.info('turning proxy off')
+    executeFlushChain().done(
+      function () {
+        logger.info('successfully flushed proxy chain')
+      },
+      function (err) {
+        logger.warn(err, 'error flushing network chain')
+      }
+    )
+  }
+  function turnProxyOn () {
+    logger.info('turning proxy on')
+    executeFlushChain()
+    .then(() => executeAddCameraForwardPort(80))
+    .then(() => executeAddCameraForwardPort(554))
+    .then(() => executeAddCameraForwardPort(8091))
+    .done(function () {
+      logger.info('successfully created proxy chain')
+    }, function (err) {
+      logger.warn(err, 'error creating network chain')
+    })
+  }
 
   function init (config) {
     localConfig = config
@@ -43,15 +108,14 @@ const deferred = require('deferred')
   }
 
   function ProcessUpdatedDeviceState (newState) {
-    debugger
     const prevState = deviceState
     deviceState = newState
-    // if proxy should be on and the camera ip or proxy state changed - then turn it on
     if (deviceState.cameraIp && deviceState.localCameraProxy && (deviceState.cameraIp !== prevState.cameraIp || deviceState.localCameraProxy !== prevState.localCameraProxy)) {
-      logger.info('turning proxy on')
+      // if proxy should be on and the camera ip or proxy state changed - then turn it on
+      turnProxyOn()
     } else if (!deviceState.localCameraProxy && (deviceState.localCameraProxy !== prevState.localCameraProxy)) {
       // if the proxy is to be turned off (and it was on) - then turn it off
-      logger.info('turning proxy off')
+      turnProxyOff()
     }
   }
 
