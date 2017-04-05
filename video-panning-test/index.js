b3cb99835448b6a624ff547e94cc4b921b88293e
Brian Kaiser <brian.kaiser@hudl.com>
2017-04-05 19:23:01 -0400

    Get end to end flow working

--- video-panning-test/index.js
+++ video-panning-test/index.js
@@ -2,28 +2,51 @@ const ffmpeg = require('fluent-ffmpeg')
 const path = require('path')
 const promisify = require('promisify-node')
 const sharp = require('sharp')
+const readline = require('readline')
 const fs = promisify('fs')
 
-// extract all the frames into a temp directory
-const movieFile = 'J:\\graybox-videos\\004L-01\\output2017-03-27_14-47-49.ts'
-const tmpDirectory = 'J:\\tmp\\pan-test\\'
+const movieFile = process.argv[2] 
+const tmpDirectory = path.dirname(movieFile)
 const imagesDirectory = path.join(tmpDirectory, 'images')
-const imagesOutput = path.join(imagesDirectory, '%05d.bmp')
+const imageExtension = '.jpg'
+const imageFileNames = '%05d' + imageExtension
+const imagesOutput = path.join(imagesDirectory, imageFileNames)
 
-const croppedDirecotry = path.join(tmpDirectory, 'cropped')
+const croppedDirectory = path.join(tmpDirectory, 'cropped')
+
+const courtPanTime = 30*8 // # of frames to pan edge to edge (speed)
+
+const mkdirSync = function (dirPath) {
+  try {
+    fs.mkdirSync(dirPath)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+}
 
 
 async function generateImages () {
+  mkdirSync(tmpDirectory)
+  mkdirSync(imagesDirectory)
+  console.log('Generating images from input...')
+  console.log('frame: 0')
   const p = new Promise((resolve, reject) => {
     ffmpeg()
       .addInput(movieFile)
       .format('image2')
       .withOutputFps(30)
+      .withOutputOptions([
+        '-q:v 2'
+      ])
+      .on('start', function(commandLine) {
+        // console.log('Spawned Ffmpeg with command: ' + commandLine);
+      })
       .on('end', function () {
         resolve()
       })
       .on('progress', function (progress) {
-        // progress.frames, progress.percent
+        readline.moveCursor(process.stdout, 0,-1)
+        console.log('frame: ' + progress.frames)
       })
       .on('error', function () {
         reject()
@@ -31,25 +54,98 @@ async function generateImages () {
       .save(imagesOutput)
   })
 
-  await p
+  return p
 }
 
 async function generateCrops () {
-  var files = await fs.readdir(imagesDirectory)
+  const cropHeight = 800 // height necessary to include the whole court
+  const cropWidth = parseInt((16/9)*cropHeight) // aspect ratio width
+  const topOffset = 500 // try to isolate the court
+  const finalVideoWidth = 1280
+  const finalVideoHeight = 720
 
-  const outPromises = files.map((f) => {
-    const outFileName = path.join(croppedDirecotry, f)
-    return sharp(path.join(imagesDirectory, f).extract({
-      left: 0,
-      top: 0,
-      width: 1280,
-      height: 720
-    }).toFile(outFileName)
-  })
-  await Promise.all(outPromises)
+  console.log('Generating crops...')
+  let curImage = 0
+  console.log('frame: ' + curImage)
+  mkdirSync(croppedDirectory)
+
+  const files = await fs.readdir(imagesDirectory)
+  const startP = Promise.resolve()
+  const finalPromise = files
+    .filter(f=>path.extname(f) === imageExtension)
+    .reduce((p, f) => {
+      return p.then(function() {
+        readline.moveCursor(process.stdout, 0,-1)
+        console.log('frame: ' + ++curImage)
+
+        const image = sharp(path.join(imagesDirectory, f))
+        return image 
+          .metadata()
+          .then(function (metadata) {
+            // make the left value pan back and forth
+            const maxLeft = metadata.width - cropWidth
+            const curLeft = parseInt(Math.cos(curImage*2*Math.PI/courtPanTime)*(maxLeft/2) + (maxLeft/2));
+            return image.extract({
+              left: curLeft,
+              top: topOffset,
+              width: cropWidth,
+              height: cropHeight
+            })
+            .resize(finalVideoWidth, finalVideoHeight)
+            .jpeg({
+              quality: 98
+            })
+            .toFile(path.join(croppedDirectory, f))
+          })
+
+    })
+  }, startP)
+  return finalPromise
 }
-//generateImages()
-generateCrops()
+
+async function generateFinalVideo () {
+    console.log('Generating final movie...')
+    console.log('frame: 0')
+    const inputImages = path.join(croppedDirectory, imageFileNames)
+    const outputFile = path.join(tmpDirectory, 'output.mp4')
+    const p = new Promise((resolve, reject) => {
+      ffmpeg()
+        .addInput(inputImages)
+        .inputFormat('image2')
+        .videoCodec('libx264')
+        .outputFps(30)
+        .outputOptions([
+          '-pix_fmt yuv420p',
+          '-crf 16',
+          '-profile:v high'
+        ])
+        .on('start', function(commandLine) {
+          // console.log('Spawned Ffmpeg with command: ' + commandLine);
+        })
+        .on('end', function () {
+          resolve()
+        })
+        .on('progress', function (progress) {
+          readline.moveCursor(process.stdout, 0,-1)
+          console.log('frame: ' + progress.frames )
+        })
+        .on('error', function (err, stdout, stderr) {
+          console.log(err,stdout, stderr)
+          reject()
+        })
+        .save(outputFile)
+  })
+
+  return p
+}
+
+generateImages()
+  .then(generateCrops)
+  .then(generateFinalVideo)
+  .then(function () {
+    console.log('COMPLETE')
+  })
+
 
 // run sharp to 'extract' the right size on each frame into another temp directory
 
